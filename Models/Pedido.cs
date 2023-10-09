3c6351806a5204ac185b14125c0724abdb5cac0c
Unagui19 <francorr199@gmail.com>
2023-10-09 10:12:48 -0300

    ramas

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -36,40 +36,6 @@ namespace EspacioEntidades
 
 
 
-        // public Pedido(string nombre, string direccion, string telefono, string datosReferenciaDireccion, string obs)
-        // {
-        //     this.nro=0;
-        //     this.estado = Estado.pendiente;
-        //     cliente = new Cliente(nombre, direccion, telefono, datosReferenciaDireccion);//esto es porque es composicion
-        //     this.Obs = obs;
-        //     cadete = new Cadete();
-        // }
-        
-        // public Pedido(int numero,string observacion,Cliente client)
-        // {
-        //     Nro=numero;
-        //     Obs = observacion;
-        //     Estado = Estado.pendiente;
-        //     Cliente = client;//esto es porque es composicion
-        //     // cadete = new Cadete();
-        // }
-        //     public Pedido()
-        // {
-        //     Nro=0;
-        //     // this.estado = Estado.pendiente;
-        //     // cliente = new Cliente();//esto es porque es composicion
-        //     // this.Obs = "";
-        //     // cadete = new Cadete();
-        // }
-
-        // public Pedido()
-        // {
-        //     this.nro=0;
-        //     // this.estado = Estado.pendiente;
-        //     // cliente = new Cliente();//esto es porque es composicion
-        //     // this.Obs = "";
-        //     // cadete = new Cadete();
-        // }
         //metodos
 
         public string verDatosCliente()
